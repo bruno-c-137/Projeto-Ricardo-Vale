f4c9020b4fab20ca054b59d9c313509952466f55
Bruno Lucas <brunnnolucas@gmail.com>
2023-06-13 10:58:27 -0300

    adicionando svgr

--- src/components/footerMain/index.tsx
+++ src/components/footerMain/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import { ReactComponent as IconFaq } from '../../assets/images/icon-faq.svg';
-import logoFooter from "../../assets/images/logoFooter.svg";
+import LogoFooter from "../../assets/images/logoFooter.svg";
 import IconPT from "../../assets/images/icons/iconPT.svg";
 import IconFaceBook from "../../assets/images/icons/iconFacebook.svg";
 import IconInstagram from "../../assets/images/icons/iconInstagram.svg";
@@ -25,36 +25,36 @@ export default function FooterMain() {
       <div className="container sm:flex sm:flex-col relative sm:px-4 lg:px-12 pt-6 pb-5">
         <div className="sm:flex-1 sm:flex sm:gap-10 lg:gap-6">
           <div className=" sm:w-4/12 lg:w-5/12">
-            <div className="">
-              <img src={logoFooter.src} alt="" />
+            <div>
+              <LogoFooter />
             </div>
           </div>
           <div className="sm:w-8/12 lg:w-7/12 sm:text-sm lg:text-base sm:flex gap-6 text-[#FFFFFF] pt-1">
-            <h1>
+            <p>
               <strong>Início</strong>
-            </h1>
-            <h1>
+            </p>
+            <p>
               <strong>Sobre</strong>
-            </h1>
+            </p>
             <div>
               <div className=" ">
-                <h1 className="hidden sm:block">
+                <p className="hidden sm:block">
                   <strong>Trabalho</strong>
-                </h1>
+                </p>
                 <button
                   className="flex items-center gap-4 sm:hidden"
                   onClick={() => toogleFaq(1)}
                 >
-                  <h1>
+                  <p>
                     <strong>Trabalho</strong>
-                  </h1>
-                  <img
-                    src={IconArrow.src}
+                  </p>
+                  <div
                     className={`block sm:hidden duration-300 ease-in ${
                       openFaq === 1 ? "-rotate-90" : "rotate-90"
                     }`}
-                    alt=""
-                  />
+                  >
+                    <IconArrow />
+                  </div>
                 </button>
               </div>
               <div
@@ -64,31 +64,31 @@ export default function FooterMain() {
                     : "max-h-[0px] overflow-hidden sm:max-h-full sm:overflow-visible"
                 }`}
               >
-                <h2 className="font-normal opacity-70">Bandeiras</h2>
-                <h2 className="font-normal opacity-70">Projetos</h2>
-                <h2 className="font-normal opacity-70">
+                <p className="font-normal opacity-70">Bandeiras</p>
+                <p className="font-normal opacity-70">Projetos</p>
+                <p className="font-normal opacity-70">
                   Apresente sua ideia de projeto
-                </h2>
+                </p>
               </div>
             </div>
             <div>
-              <h1 className=" hidden sm:block">
+              <p className=" hidden sm:block">
                 <strong>Midia</strong>
-              </h1>
+              </p>
               <button
                 className="flex items-center gap-4 sm:hidden"
                 onClick={() => toogleFaq(2)}
               >
-                <h1>
+                <p>
                   <strong>Midia</strong>
-                </h1>
-                <img
-                  src={IconArrow.src}
+                </p>
+                <div
                   className={`block sm:hidden duration-300 ease-in ${
                     openFaq === 2 ? "-rotate-90" : "rotate-90"
                   }`}
-                  alt=""
-                />
+                >
+                  <IconArrow />
+                </div>
               </button>
               <div
                 className={`text-xs pt-1 duration-300 ease-in block ${
@@ -97,33 +97,31 @@ export default function FooterMain() {
                     : "max-h-[0px] overflow-hidden sm:max-h-full sm:overflow-visible"
                 }`}
               >
-                <h2 className="font-normal opacity-70">Notícias</h2>
-                <h2 className="font-normal opacity-70">Artigos</h2>
-                <h2 className="font-normal opacity-70">Conteúdos de áudio</h2>
-                <h2 className="font-normal opacity-70">Flickr</h2>
-                <h2 className="font-normal opacity-70">
-                  Material para download
-                </h2>
-                <h2 className="font-normal opacity-70">Agenda</h2>
-                <h2 className="font-normal opacity-70">Amigos do Ricardo</h2>
+                <p className="font-normal opacity-70">Notícias</p>
+                <p className="font-normal opacity-70">Artigos</p>
+                <p className="font-normal opacity-70">Conteúdos de áudio</p>
+                <p className="font-normal opacity-70">Flickr</p>
+                <p className="font-normal opacity-70">Material para download</p>
+                <p className="font-normal opacity-70">Agenda</p>
+                <p className="font-normal opacity-70">Amigos do Ricardo</p>
               </div>
             </div>
-            <h1>
+            <p>
               <strong>Comunidade</strong>
-            </h1>
-            <h1>
+            </p>
+            <p>
               <strong>Contato</strong>
-            </h1>
+            </p>
           </div>
         </div>
         <div className="sm:flex justify-between pt-5 sm:pt-12">
           <div className="flex items-center gap-7">
-            <img src={IconPT.src} alt="" />
-            <img src={IconInstagram.src} alt="" />
-            <img src={IconFaceBook.src} alt="" />
-            <img src={IconYoutube.src} alt="" />
-            <img src={IconFace.src} alt="" />
-            <img src={IconTwitter.src} alt="" />
+            <IconPT />
+            <IconInstagram />
+            <IconFaceBook />
+            <IconYoutube />
+            <IconFace />
+            <IconTwitter />
           </div>
           <div className="text-[10px] text-[#FFFFFF] pt-3 sm:pt-0">
             <p>Copyright © 2023 Ricardo Vale - Todos os direitos reservados</p>
