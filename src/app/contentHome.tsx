3108fbc12f6c43131018f63295dae205abd69d45
Bruno Lucas <brunnnolucas@gmail.com>
2023-06-14 00:53:37 -0300

    adicionando swiper

---
+++ src/app/contentHome.tsx
@@ -0,0 +1,35 @@
+import ImgTransporte from "@/assets/images/img-transporte.svg";
+export const boxConquistas: any = [
+  {
+    text: "Tornou todos os assentos dos transporte público  PREFERENCIAIS.",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "Lei que obriga o governo do DF comprar 30% da alimentação escolar da agricultura familiar.",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "PL que combate o machismo e valoriza as mulheres nas escolas públicas.",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "Institui o programa das bibliotecas comunitárias em todo o DF.",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "Lei que obriga empresas a destinar 2% das vagas para a população de rua, em obras e contratos com o GDF.",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "Criou a semana da democracia a ser comemorada em todas as escolas do DF",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "Lei que permite a secretaria de segurança divulgar os índices e locais de maior violência contra mulheres, LGBTs, negros, moradores de rua e jovens no DF.",
+    img: <ImgTransporte />,
+  },
+  {
+    text: "Lei dos Boleiros. PL que permite o GDF incentivar o esporte amador no DF, com pagamento de Arbitragens e aquisição de materiais.",
+    img: <ImgTransporte />,
+  },
+];
